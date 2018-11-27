723a2382666ada9f7b9e7662e590e91ebddc9d2f
Alexandre Peccard <alexandre.peccard@gmail.com>
2018-11-27 11:41:14 +0100

    scheme 2nd version

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,14 +1,13 @@
 class CartsController < ApplicationController
   def add_item
     @cart.add(item_id) if Item.exists?(item_id)
-
-    redirect_to cart_path
+    # redirect_to cart_path
     end
 
     def remove_item
       @cart.delete(item_id)
 
-      redirect_to cart_path
+      # redirect_to cart_path
     end
 
     def item_id
